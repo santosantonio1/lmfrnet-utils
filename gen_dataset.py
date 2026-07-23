2d6462b7475050a017f45509cc70cbe697576b81
Antônio dos Santos <antonio.s001@edu.pucrs.br>
2026-07-23 17:45:25 -0300

    added the expected prediction comments at the generated .h files

--- gen_dataset.py
+++ gen_dataset.py
@@ -2,7 +2,10 @@ import argparse
 import os
 import pickle
 import numpy as np
+import torch
 import torchvision.transforms as transforms
+
+from fused_model import load_fused_model
 
 DATA_PATH = os.path.join(
     os.path.dirname(os.path.abspath(__file__)),
@@ -10,6 +13,9 @@ DATA_PATH = os.path.join(
 )
 OUTPUT_DIR = './dataset-quant'
 QUANTIZATION_SCALE = 8192
+
+CLASSES = ('plane', 'car', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck')
 
 transform = transforms.Compose([
     transforms.ToTensor(),
@@ -25,13 +31,16 @@ def unpickle(file):
         return pickle.load(f, encoding='bytes')
 
 
-def quantize(image):
+def preprocess(image):
     img = image.reshape(3, 32, 32).transpose(1, 2, 0)
-    img = transform(img).unsqueeze(0)
-    return (img.permute(0, 2, 3, 1).numpy().flatten() * QUANTIZATION_SCALE).astype(np.int32)
+    return transform(img)
 
 
-def generate_header(name, values, root=OUTPUT_DIR):
+def quantize(img):
+    return (img.unsqueeze(0).permute(0, 2, 3, 1).numpy().flatten() * QUANTIZATION_SCALE).astype(np.int32)
+
+
+def generate_header(name, values, label, prediction, root=OUTPUT_DIR):
     os.makedirs(root, exist_ok=True)
 
     filename = f'{root}/{name}.h'
@@ -39,6 +48,8 @@ def generate_header(name, values, root=OUTPUT_DIR):
     with open(filename, 'w') as f:
         f.write(f'#ifndef __{header_guard}_H__\n')
         f.write(f'#define __{header_guard}_H__\n\n')
+        f.write(f'// class: {label} ({CLASSES[label]})\n')
+        f.write(f'// predicted class: {prediction} (val = {prediction}, {CLASSES[prediction]})\n')
         f.write(f'const int {name}[{values.size}] = {{\n')
         f.write(',\n'.join(map(str, values)))
         f.write('\n};\n')
@@ -54,6 +65,12 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     test_batch = unpickle(DATA_PATH)
+    labels = test_batch[b'labels'][:args.num_images]
+    imgs = torch.stack([preprocess(image) for image in test_batch[b'data'][:args.num_images]])
 
-    for n, image in enumerate(test_batch[b'data'][:args.num_images], start=1):
-        generate_header(f'image{n}', quantize(image), root=args.output_dir)
+    model = load_fused_model()
+    with torch.no_grad():
+        predictions = model(imgs).argmax(dim=1).tolist()
+
+    for n, (img, label, prediction) in enumerate(zip(imgs, labels, predictions), start=1):
+        generate_header(f'image{n}', quantize(img), label, prediction, root=args.output_dir)
